76fb0898252af926e479e14677bf35c8f1d2850f
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-23 09:46:19 -0500

    lint fixes

--- src/get-implied-package-json.mjs
+++ src/get-implied-package-json.mjs
@@ -2,7 +2,7 @@ import createError from 'http-errors'
 
 import { getPackageJSON } from './get-package-json'
 
-const getImpliedPackageJSON = async ({ callDesc, req }) => {
+const getImpliedPackageJSON = async({ callDesc, req }) => {
   const currDir = req.get('X-CWD')
   if (currDir === undefined) {
     throw createError.BadRequest(`Called '${callDesc}' for implied project, but 'X-CWD' header not found.`)
--- src/test/get-implied-package-json.test.mjs
+++ src/test/get-implied-package-json.test.mjs
@@ -4,7 +4,7 @@ import * as fsPath from 'node:path'
 import { getImpliedPackageJSON } from '../get-implied-package-json'
 
 const reqMock = (currDir) => ({
-  get: (header) => header === 'X-CWD' ? currDir : undefined
+  get : (header) => header === 'X-CWD' ? currDir : undefined
 })
 
 describe('getImpliedPackageJSON', () => {
@@ -12,7 +12,7 @@ describe('getImpliedPackageJSON', () => {
     [fsPath.resolve(fsPath.join(__dirname, 'data', 'orgA', 'proj1')), '@orgA/proj1'],
     [fsPath.resolve(fsPath.join(__dirname, 'data', 'orgA', 'proj1', 'subdir')), '@orgA/proj1'],
     [__dirname, '@liquid-labs/liq-projects-lib']
-  ])("'%s' -> '%s'", async (currDir, expectedProjectName) => {
+  ])("'%s' -> '%s'", async(currDir, expectedProjectName) => {
     expect((await getImpliedPackageJSON({ req : reqMock(currDir) })).name).toBe(expectedProjectName)
   })
 })
