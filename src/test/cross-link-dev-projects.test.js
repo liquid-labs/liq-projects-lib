c2d4585d612b6c6c1694802d341013494423b574
Zane Rockenbaugh <zane@liquid-labs.com>
2023-10-26 08:55:47 -0500

    updated unit test for new implementation

--- src/test/cross-link-dev-projects.test.js
+++ src/test/cross-link-dev-projects.test.js
@@ -5,12 +5,25 @@ import { crossLinkDevProjects } from '../cross-link-dev-projects'
 
 describe('crossLinkDevProjects', () => {
   test('can deal with lack of dependencies', () => {
+    const proj1Path = fsPath.join(__dirname, 'data', 'orgA', 'proj1')
+
+    const appMock = {
+      ext: {
+        _liqProjects: {
+          playgroundMonitor: {
+            getProjectData: () => ({ projectPath: proj1Path })
+          }
+        }
+      }
+    }
+
     const playground = fsPath.resolve(fsPath.join(__dirname, 'data'))
     expect(() => {
       const links = crossLinkDevProjects({
+        app: appMock,
         dryRun   : true,
         playground,
-        projects : ['orgA/proj1']
+        projects : ['@orgA/proj1']
       })
       expect(links).toHaveLength(0)
     }).not.toThrow()
